3aaa51ad745f3e9bfd97b913d7122e1fad93966a
praveenmedikonda <43464473+praveenmedikonda@users.noreply.github.com>
2018-11-27 18:39:31 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,3 @@
+from sklearn.datasets import fetch_20newsgroups
+newsgroups_train = fetch_20newsgroups(subset='train', shuffle = True)
+newsgroups_test = fetch_20newsgroups(subset='test', shuffle = True)
